882a6d6b5540dccbd26ec44b82cc6703331b0b61
tony <azntonyzxd@gmail.com>
2021-07-20 16:22:29 -0400

    day 1

---
+++ com_ss_july/src/com_ss_july_week1/com_ss_ex1.java
@@ -0,0 +1,32 @@
+package com_ss_july_week1;
+
+public class com_ss_ex1 {
+
+	public static void main(String[] args) {
+		System.out.print("1)\r\n" + 
+				"*\r\n" + 
+				"**\r\n" + 
+				"***\r\n" + 
+				"****\r\n" + 
+				".........\r\n" + 
+				"2)\r\n" + 
+				"..........\r\n" + 
+				"****\r\n" + 
+				"***\r\n" + 
+				"**\r\n" + 
+				"*\r\n" + 
+				"3)\r\n" + 
+				"     *\r\n" + 
+				"    ***\r\n" + 
+				"   *****\r\n" + 
+				"  *******\r\n" + 
+				"...........\r\n" + 
+				"4)\r\n" + 
+				"............\r\n" + 
+				"  *******\r\n" + 
+				"   *****\r\n" + 
+				"    ***\r\n" + 
+				"     *");
+	}
+
+}
---
+++ com_ss_july/src/com_ss_july_week1/com_ss_ex2.java
@@ -0,0 +1,29 @@
+package com_ss_july_week1;
+
+import java.util.Scanner;
+
+public class com_ss_ex2 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int count = 0, num, numGuess = 0;
+		num = (int) (Math.random() * 100);
+		System.out.print(num);
+		while(count < 5) {
+			System.out.println("Guess a number between 1-100: ");
+			numGuess = in.nextInt(); //can add error checker here
+			if(numGuess < (num + 10) && numGuess > (num - 10)) {
+				System.out.println("Correct answer was: " + num);
+				System.exit(0);
+			} else {
+				System.out.println("Keep guessing!");
+				count++;
+			}
+		}
+		System.out.println("Sorry you guessed too many times. The correct answer was: " + num);
+		in.close();
+		System.exit(0);
+	}
+}
+
+}
