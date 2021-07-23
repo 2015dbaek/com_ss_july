6e24434ddf91afc79dc9e047c98c8378d68f98b2
tony <azntonyzxd@gmail.com>
2021-07-23 18:28:37 -0400

    day4

---
+++ com_ss_july/src/com_ss_july_day4/Ex1Day4.java
@@ -0,0 +1,20 @@
+package com_ss_july_day4;
+
+public class Ex1Day4 {
+
+	volatile public static Ex1Day4 instance = null;
+	
+	private Ex1Day4() { //private constructor
+		
+	}
+	
+	/*synchronized method sync*/ public Ex1Day4 getInstance() {
+		synchronized(instance) {
+			if(instance == null) {
+				instance = new Ex1Day4();
+			}
+		}
+		return instance;
+	}
+	
+}
